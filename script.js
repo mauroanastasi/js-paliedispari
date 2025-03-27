9119049c78ac79022246b946ae73b6f3330910c7
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-28 00:29:27 +0100

    [UPD] divido il problema

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-// definisco la funzione
-function reverseWord (word){
-    let parola = word.split(``).reverse().join(``)
-    if(parola === word){
-        console.log(`la parola è palindroma`)
-    }
-    else{
-        console.log(`la parola non è palindroma`)
-    }
+// // definisco la funzione
+// function reverseWord (word){
+//     let parola = word.split(``).reverse().join(``)
+//     if(parola === word){
+//         console.log(`la parola è palindroma`)
+//     }
+//     else{
+//         console.log(`la parola non è palindroma`)
+//     }
 
-}
+// }
 
-// corpo del programma
-let parola = prompt(`inserisci la tua parola:`)
-reverseWord(parola)
+
+// // corpo del programma
+// let parola = prompt(`inserisci la tua parola:`)
+// reverseWord(parola)
+
+
+
+
+
+
+
+
+
+// Funzione per generare un numero casuale tra 1 e 5
+
+// Funzione per stabilire se un numero è pari o dispari
+
+// Corpo del programma
+
+
+// Sommiamo i due numeri
+
+
+// Stabiliamo se la somma è pari o dispari
+
+// Dichiariamo chi ha vinto
