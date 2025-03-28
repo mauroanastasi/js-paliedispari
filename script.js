121b768e20917cb926313927107761cc21e448c3
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-28 08:22:51 +0100

    [UPD] creo funzione per generare numeri random da parte del pc

--- script.js
+++ script.js
@@ -24,12 +24,16 @@
 
 
 // Funzione per generare un numero casuale tra 1 e 5
-
+function generoRandom (){
+    let numPc = Math.floor(Math.random() + 10)+ 1;
+    console.log(numPc)
+};
 // Funzione per stabilire se un numero è pari o dispari
 
 // Corpo del programma
-let pariDispari = prompt(`scegli se pari o dispari`)
-let numUser = prompt(`scegli il tuo numero`)
+let pariDispari = prompt(`scegli se pari o dispari`);
+let numUser = prompt(`scegli il tuo numero`);
+generoRandom();
 
 // Sommiamo i due numeri
 
